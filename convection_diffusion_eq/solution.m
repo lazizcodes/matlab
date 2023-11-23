109cc430b794e2e0003d0d1fe3e0ae3068897a84
lazizcodes <lazizabdullaev02@gmail.com>
2023-11-23 20:38:00 +0800

    finalize solution

--- convection_diffusion_eq/solution.m
+++ convection_diffusion_eq/solution.m
@@ -1,124 +1,94 @@
-% ### SOLUTION ###
-
-[tri, x, y] = generateTriangularMesh(60);
+[tri, x, y] = generateTriangularMesh(30);
+centroids = getTriangularMeshCentroids(tri, x, y);
 trimesh(tri, x, y); axis('equal');
 
+% Add centroids as points
+hold on;
+scatter(centroids(:, 1), centroids(:, 2), 5, 'r', 'filled');
+hold off;
+
 % Set up parameters
+mu = 0.1;
 numNodes = numel(x);
 numTriangles = size(tri, 1);
-dt = 0.0005; % time step
+dt = 0.01; % time step
 finalTime = 1; % final simulation time
-programLifeTime = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
 
 % Initialize solution
-u = zeros(numNodes, 1);
+u = zeros(numTriangles, 1);
 
 % Set initial condition inside the domain
-for i = 1:numNodes
-    u(i) = sin(pi * (x(i)^2 + y(i)^2)) * ((x(i) - 1)^2 + y(i)^2 - 9);
+for i = 1:numTriangles
+    centroid = centroids(i, :);
+    u(i) = sin(pi * (centroid(1)^2 + centroid(2)^2)) * ((centroid(1) - 1)^2 + centroid(2)^2 - 9);
 end
-
-% Enforce boundary condition (u = 0 on the boundary)
-u = enforceBoundaryCondition(u, tri, x, y);
 
 % Velocity field
 v = [1, 2];
 
 % Create the initial figure
 figure;
-h = trisurf(tri, x, y, u, 'EdgeColor', 'k', 'FaceColor', 'interp');
+% h = trisurf(tri, centroids(:, 1), centroids(:, 2), u, 'EdgeColor', 'k', 'FaceColor', 'interp');
+scatterObj = scatter(centroids(:, 1), centroids(:, 2), 70, u, 'filled');
 xlabel('X-axis');
 ylabel('Y-axis');
 zlabel('Solution (u)');
 title(['Numerical Solution at t = 0']);
+colorbar;
 drawnow;
 
-% Time-stepping loop
-for t = dt:dt:finalTime
-    % Laplacian term
-    laplacian_u = computeLaplacian(tri, x, y, u);
-    
+t = dt;
+while t < finalTime
+    disp(t)
     % Loop over triangles for FVM
     for i = 1:numTriangles
         nodes = tri(i, :);
-        
-        % Calculate face normals
-        normal = computeFaceNormals(x(nodes), y(nodes));
-        
-        % Compute flux term
-        flux = (v * normal') / norm(normal) * u(nodes);
 
-        
-        % Update solution using FVM
-        u(nodes) = u(nodes) + (dt / polyarea(x(nodes), y(nodes))) * (0.1 * laplacian_u(i) - flux);
+        % Calculate the centroid of the current triangle
+        centroid = centroids(i, :);
+
+        % Calculate the area of the current triangle
+        triangleArea = polyarea(x(nodes), y(nodes));
+
+        % Initialize the summation term
+        summationTerm = 0;
+        flag = 0;
+        % Iterate over edges of the current triangle
+        for j = 1:3
+            edge = nodes([j, mod(j, 3) + 1]); % Current edge as [start, end]
+            edgeLength = norm([x(edge(1)) - x(edge(2)), y(edge(1)) - y(edge(2))]);
+
+            % Find the index of the adjacent triangle sharing the current edge
+            % adjacentTriangle = find(sum(ismember(tri(:, 1:3), edge), 2) == 2 & ~ismember(1:numTriangles, i));
+            adj = findAdjTri(tri, edge, i);
+
+            if adj < 0
+                flag = 1;
+                break;
+            end
+
+
+            nrm = centroids(adj, :) - centroids(i, :);
+            vij = dot(v, nrm / norm(nrm));
+
+            % Accumulate the summation term
+            summationTerm = summationTerm + edgeLength * (max(vij, 0) * u(i) + min(vij, 0) * u(adj) - mu * (u(adj) - u(i)) / norm(nrm));
+        end
+
+        if flag < 1
+            % Update the solution using the given update rule
+            u(i) = u(i) - (dt / triangleArea) * summationTerm;
+        end
     end
 
-    % Enforce boundary condition (u = 0 on the boundary)
-    u = enforceBoundaryCondition(u, tri, x, y);
-
-    if ismember(t, finalTime * programLifeTime)
-        % Update the figure
-        set(h, 'Vertices', [x(:), y(:), u(:)]);
-        title(['Numerical Solution at t = ' num2str(t)]);
-        drawnow;
-    end
+    scatter(centroids(:, 1), centroids(:, 2), 70, u, 'filled');
+    title(['Numerical Solution at t = ' num2str(t)]);
+    colorbar
+    drawnow;
+    t = t + dt;
 end
 
 % ### HELPER FUNCTIONS ###
-
-function u = enforceBoundaryCondition(u, tri, x, y)
-    numNodes = numel(x);
-    numTriangles = size(tri, 1);
-
-    % Loop over triangles to identify boundary nodes
-    for i = 1:numTriangles
-        nodes = tri(i, :);
-        
-        % Identify nodes on the boundary
-        boundaryNodes = nodes(x(nodes).^2 + y(nodes).^2 == 1 | ...
-                               (x(nodes)-1).^2 + y(nodes).^2 == 9);
-        
-        % Set solution to zero on boundary nodes
-        u(boundaryNodes) = 0;
-    end
-end
-
-function laplacian_u = computeLaplacian(tri, x, y, u)
-    % Compute Laplacian of the solution at each triangle using linear shape functions
-    
-    numTriangles = size(tri, 1);
-    laplacian_u = zeros(numTriangles, 1);
-
-    for i = 1:numTriangles
-        nodes = tri(i, :);
-        area = polyarea(x(nodes), y(nodes));
-        
-        % Linear shape functions for the Laplacian
-        N1 = 1 / (2 * area) * (y(nodes(2)) - y(nodes(3)));
-        N2 = 1 / (2 * area) * (y(nodes(3)) - y(nodes(1)));
-        N3 = 1 / (2 * area) * (y(nodes(1)) - y(nodes(2)));
-        
-        % Calculate Laplacian using linear shape functions
-        laplacian_u(i) = N1 * u(nodes(1)) + N2 * u(nodes(2)) + N3 * u(nodes(3));
-    end
-end
-
-function faceNormals = computeFaceNormals(x, y)
-    % Compute outward-facing normal vectors for each face of a triangle
-    
-    numNodes = length(x);
-    faceNormals = zeros(numNodes, 2);
-
-    for i = 1:numNodes
-        j = mod(i, numNodes) + 1; % Next node in the sequence
-        dx = x(j) - x(i);
-        dy = y(j) - y(i);
-        
-        % Compute outward-facing normal vector
-        faceNormals(i, :) = [-dy, dx] / norm([dx, dy]);
-    end
-end
-
 
 function centroids = getTriangularMeshCentroids(tri, x, y)
 centroids = zeros(size(tri, 1), 2);
@@ -196,4 +166,23 @@ tri = tri(setdiff(1:numRows,elemsToRemove),:);
 
 x = x(1:end-1);
 y = y(1:end-1);
+end
+
+function matchingRow = findAdjTri(tri, edge, j)
+% Initialize matching row as an empty array
+matchingRow = -1;
+
+target1 = edge(1);
+target2 = edge(2);
+
+% Iterate over each row
+for i = 1:size(tri, 1)
+    % Check if both targets are present in the current row
+    if i ~= j && ismember(target1, tri(i, :)) && ismember(target2, tri(i, :))
+        % Set matchingRow to the current row index
+        matchingRow = i;
+        % Break the loop since we found a match
+        break;
+    end
+end
 end
